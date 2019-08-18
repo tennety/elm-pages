09f77dc7dfa1ac95c7fef3bee53d62a2b19c9a41
Matthew Griffith <mdg.griffith@gmail.com>
2019-08-18 18:13:12 -0400

    rewrite elm.json so it can be used in generated files

--- generator/src/elm-pages.js
+++ generator/src/elm-pages.js
@@ -2,6 +2,7 @@
 
 const { Elm } = require("./Main.js");
 const { version } = require("../../package.json");
+
 const fs = require("fs");
 const glob = require("glob");
 const develop = require("./develop.js");
@@ -29,56 +30,80 @@ function run() {
   // New File Generation
   // Uncomment to generate the new file
   // const myElmFile = generate.run("site/content/")
-  // fs.mkdir('./site/my', { recursive: true }, (err) => {
+  // fs.mkdirSync('./site/my', { recursive: true }, (err) => {
   //   if (err) throw err;
   // });
   // fs.writeFileSync("./site/my/My.elm", myElmFile);
 
-  const content = glob.sync(contentGlobPath, {}).map(unpackFile);
-  const markdownContent = glob
-    .sync("content/**/*.md", {})
-    .map(unpackFile)
-    .map(({ path, contents }) => {
-      return parseMarkdown(path, contents);
-    });
-  const images = glob
-    .sync("images/**/*", {})
-    .filter(imagePath => !fs.lstatSync(imagePath).isDirectory());
+  // Create our stubbed in version of the file
+  const myInternalElmFile = generate.runInternal("site/content/")
 
-  let app = Elm.Main.init({
-    flags: {
-      argv: process.argv,
-      versionMessage: version,
-      content,
-      markdownContent,
-      images
-    }
-  });
+  // Read elm.json file and rewrite it can live in `elm-stuff`
+  // with a new, stubbed `My.elm` file
+  var elmJson = JSON.parse(fs.readFileSync("./site/elm.json").toString())
 
-  app.ports.printAndExitSuccess.subscribe(message => {
-    console.log(message);
-    process.exit(0);
-  });
+  var newElmJson = rewriteElmJson(elmJson)
 
-  app.ports.printAndExitFailure.subscribe(message => {
-    console.log(message);
-    process.exit(1);
+  // create target dirs
+  fs.mkdirSync('./site/elm-stuff/generated-code/dillonkearns/elm-pages', { recursive: true }, (err) => {
+    if (err) throw err;
   });
+  // write new elm.json
+  fs.writeFileSync("./site/elm-stuff/generated-code/dillonkearns/elm-pages/elm.json", JSON.stringify(newElmJson));
+  // write stubbed in My.elm
+  fs.writeFileSync("./site/elm-stuff/generated-code/dillonkearns/elm-pages/My.elm", myInternalElmFile);
 
-  app.ports.writeFile.subscribe(contents => {
-    fs.writeFileSync("./gen/RawContent.elm", contents.rawContent);
-    fs.writeFileSync("./src/js/image-assets.js", contents.imageAssets);
-    console.log("elm-pages DONE");
-    if (contents.watch) {
-      startWatchIfNeeded();
-      develop.start({ routes: contents.routes, debug: contents.debug });
-    } else {
-      develop.run(
-        { routes: contents.routes, fileContents: contents.fileContents },
-        () => { }
-      );
-    }
-  });
+  // Then you can cd to ./site/elm-stuff/generated-code/dillonkearns/elm-pages/
+  // and run elm make ../../../../OriginalMain.elm
+  // and it'll create a platform worker version instead.
+
+
+
+  // const content = glob.sync(contentGlobPath, {}).map(unpackFile);
+  // const markdownContent = glob
+  //   .sync("content/**/*.md", {})
+  //   .map(unpackFile)
+  //   .map(({ path, contents }) => {
+  //     return parseMarkdown(path, contents);
+  //   });
+  // const images = glob
+  //   .sync("images/**/*", {})
+  //   .filter(imagePath => !fs.lstatSync(imagePath).isDirectory());
+
+  // let app = Elm.Main.init({
+  //   flags: {
+  //     argv: process.argv,
+  //     versionMessage: version,
+  //     content,
+  //     markdownContent,
+  //     images
+  //   }
+  // });
+
+  // app.ports.printAndExitSuccess.subscribe(message => {
+  //   console.log(message);
+  //   process.exit(0);
+  // });
+
+  // app.ports.printAndExitFailure.subscribe(message => {
+  //   console.log(message);
+  //   process.exit(1);
+  // });
+
+  // app.ports.writeFile.subscribe(contents => {
+  //   fs.writeFileSync("./gen/RawContent.elm", contents.rawContent);
+  //   fs.writeFileSync("./src/js/image-assets.js", contents.imageAssets);
+  //   console.log("elm-pages DONE");
+  //   if (contents.watch) {
+  //     startWatchIfNeeded();
+  //     develop.start({ routes: contents.routes, debug: contents.debug });
+  //   } else {
+  //     develop.run(
+  //       { routes: contents.routes, fileContents: contents.fileContents },
+  //       () => { }
+  //     );
+  //   }
+  // });
 }
 
 run();
@@ -99,4 +124,24 @@ function startWatchIfNeeded() {
         console.log("Done!");
       });
   }
+}
+
+// 
+function rewriteElmJson(elmJson) {
+  // The internal generated file will be at:
+  // ./elm-stuff/generated-code/dillonkearns/elm-pages
+  // So, we need to take the existing elmJson and
+  // 1. remove existing path that looks at `My.elm`
+  elmJson["source-directories"] =
+    elmJson["source-directories"].filter((item) => {
+      return item != "my"
+    })
+  // 2. prepend ../../../ to remaining 
+  elmJson["source-directories"] = elmJson["source-directories"].map((item) => {
+    return "../../../../" + item
+  })
+  // 3. add our own secret My.elm module 😈
+  elmJson["source-directories"].push(".")
+  return elmJson
+
 }
